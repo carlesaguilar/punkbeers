114afdd43fe5f95b1bcc0a4dbc9ab9c0cf8ac9bb
Carles Aguilar <carles@carlesav.dev>
2022-05-05 21:19:41 +0200

    first commit

---
+++ app/src/main/java/dev/carlesav/punkbeers/MainActivity.kt
@@ -0,0 +1,41 @@
+package dev.carlesav.punkbeers
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.carlesav.punkbeers.ui.theme.PunkBeersTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PunkBeersTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PunkBeersTheme {
+        Greeting("Android")
+    }
+}
